98c9a7176b50b4bb2ced91898362a123b425b77e
mlgmontage <kettleofrassel1@gmail.com>
2020-04-26 05:10:29 -0700

    initial

--- sketch/main.js
+++ sketch/main.js
@@ -1,10 +1,24 @@
+let height_slider;
+let width_slider;
+let divElem;
+let colorPicker;
+let rect_width = 30;
+let rect_height = 30;
+
 function setup() {
   createCanvas(300, 400);
+  colorPicker = createColorPicker("#000000");
+
+  divElem = createDiv("-");
+  width_slider = createSlider(0, width, 50);
+
+  height_slider = createSlider(0, height, 50);
 }
 
 function draw() {
   background(255);
 
-  rect(10, 10, 40, 50);
-  fill(0);
+  rect(10, 10, width_slider.value(), height_slider.value());
+  fill(colorPicker.color());
+  noStroke();
 }
