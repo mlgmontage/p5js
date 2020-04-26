431a1f9e70d7e3c47be71c78dc309fa76b2ca767
mlgmontage <kettleofrassel1@gmail.com>
2020-04-26 11:31:41 -0700

    mapping ranges

--- sketch/main.js
+++ sketch/main.js
@@ -1,10 +1,25 @@
 function setup() {
-  createCanvas(300, 400);
+  createCanvas(500, 400);
 }
+
+let a = 0;
 
 function draw() {
   background(255);
+  let h = map(sin(a), -1, 1, 0, 255);
+  a += 0.05;
 
-  rect(10, 10, 40, 50);
-  fill(0);
+  rect(0, 0, h, h);
+  fill(color(h, 0, 0)); // red
+
+  rect(0, height, h, -h);
+  fill(color(0, h, 0)); // green
+
+  rect(width, 0, -h, h);
+  fill(color(0, 0, h)); // blue
+
+  rect(width, height, -h, -h);
+  fill(color(h, h, h)); // white
+
+  noStroke();
 }
