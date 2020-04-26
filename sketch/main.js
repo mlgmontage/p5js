b1ac52b5f4c6b7945535c9476018410ac47bb293
mlgmontage <kettleofrassel1@gmail.com>
2020-04-26 07:51:59 -0700

    angles

--- sketch/main.js
+++ sketch/main.js
@@ -1,10 +1,25 @@
+let angle = 0;
+let w = 24;
 function setup() {
-  createCanvas(300, 400);
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(255);
+  background(0);
 
-  rect(10, 10, 40, 50);
-  fill(0);
+  translate(width / 2, height / 2);
+  rectMode(CENTER);
+
+  let offset = 0;
+
+  for (let i = 0; i < width; i += 24) {
+    let a = angle + offset;
+    let h = map(sin(a), -1, 1, 0, 100);
+    fill(255);
+    rect(i - width / 2, 0, w, h);
+
+    offset += 0.134;
+  }
+
+  angle += 0.1;
 }
