bec076588f40d2fb39df6120fda8f89973be1ad4
mlgmontage <kettleofrassel1@gmail.com>
2020-04-26 05:21:50 -0700

    array shuffling

--- sketch/main.js
+++ sketch/main.js
@@ -1,10 +1,25 @@
+let data = [];
+
+for (let i = 0; i < 50; i++) {
+  data.push(parseInt(Math.random() * 400) + 100);
+}
+
+setInterval(function () {
+  shuffle(data, true);
+}, 800);
+
 function setup() {
-  createCanvas(300, 400);
+  createCanvas(500, 500);
 }
 
 function draw() {
   background(255);
 
-  rect(10, 10, 40, 50);
-  fill(0);
+  for (let i = 0; i < 50; i++) {
+    rect(10 * (i + 1), height - data[i], 10, data[i]);
+    fill(0);
+  }
+
+  noStroke();
+  fill(255);
 }
