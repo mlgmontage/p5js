aa2789e25e4d86dc07438826210752cf96bb4837
mlgmontage <kettleofrassel1@gmail.com>
2020-04-26 09:39:08 -0700

    translator

--- sketch/main.js
+++ sketch/main.js
@@ -1,10 +1,32 @@
+let toggle = false;
+
+function mousePressed() {
+  print("pressed");
+  toggle = true;
+}
+
+function mouseReleased() {
+  print("released");
+  toggle = false;
+}
+
+let last_postion = [0, 0];
+
 function setup() {
-  createCanvas(300, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
   background(255);
 
+  if (toggle) {
+    last_postion[0] = mouseX;
+    last_postion[1] = mouseY;
+    translate(last_postion[0], last_postion[1]);
+  } else {
+    translate(last_postion[0], last_postion[1]);
+  }
   rect(10, 10, 40, 50);
+  rect(100, 100, 10, 10);
   fill(0);
 }
